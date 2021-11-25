176adb7646c9c7ec2b20060a6fb3600bf1e0c9b2
mxr <eetu.kivelae@gmail.com>
2021-11-25 04:52:41 +0200

    Made capybara command post a tailored gif for each day

--- src/commands/cmd/capybara.rs
+++ src/commands/cmd/capybara.rs
@@ -4,21 +4,41 @@ use serenity::client::Context;
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::interactions::application_command::ApplicationCommandInteraction;
 use serenity::Error;
+use chrono::prelude::*;
+use crate::constants::EMBED_COLOUR;
 
 pub struct Capybara;
 
-const CAPYBARA_GIFS: [&str; 1] = [
-  "https://tenor.com/view/capybara-bucket-sit-spa-capybara-bucket-gif-23305453"
-];
+const FILE_URL: &str = "https://mxrr.dev/files/capybara-gifs/";
+const FILE_PREFIX: &str = "cp_";
+
 
 #[async_trait]
 impl Command for Capybara {
 
   async fn execute(ctx: &Context, command: ApplicationCommandInteraction) -> Result<(), Error> {
+    let filename = match Local::now().weekday() {
+      Weekday::Mon => "monday",
+      Weekday::Tue =>  "tuesday",
+      Weekday::Wed => "wednesday",
+      Weekday::Thu => "thursday",
+      Weekday::Fri => "friday",
+      Weekday::Sat => "saturday",
+      Weekday::Sun => "sunday",
+    };
     match command
     .edit_original_interaction_response(&ctx.http, |response| {
       response
-        .content(CAPYBARA_GIFS[0])
+        .create_embed(|embed| {
+          embed
+            .image(format!(
+              "{url}{prefix}{filename}.gif", 
+              url = FILE_URL,
+              prefix = FILE_PREFIX,
+              filename = filename
+            ))
+            .colour(EMBED_COLOUR)
+        })
     }).await
     {
       Ok(_) => Ok(()),
@@ -29,7 +49,7 @@ impl Command for Capybara {
   fn info(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
     command
       .name("capybara")
-      .description("Post a random capybara gif")
+      .description("Post today's capybara gif")
   }
 
 }
